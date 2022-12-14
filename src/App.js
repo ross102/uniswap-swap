beb72665213e5d8a4ffd1dcee06ad8df5be22899
Ifeanyi Ndu <indu@fsdhgroup.com>
2022-12-14 04:19:14 +0100

    added more components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
 
 import { useState , useEffect } from 'react';
 import { ethers } from "ethers";
-import PageButton from "./components/Pagebutton";
-import ConnectButton from './components/Connectbutton';
 import './App.css';
 import { GearFill } from 'react-bootstrap-icons';
+
+
+import PageButton from "./components/Pagebutton";
+import ConnectButton from './components/Connectbutton';
+import ConfigModal from './components/Configmodal';
+import { Currency } from '@uniswap/sdk';
+import { BeatLoader } from 'react-spinners';
+
+import { getWethContract, getUniContract } from "./ContractService";
 
 
 function App() {
   const [provider, setProvider] = useState(undefined);
   const [signer, setSigner] = useState(undefined);
   const [signerAddress, setSignerAddress] = useState(undefined);
-  
+
+  const [slippageAmount, setSlippageAmount] = useState(2);
+  const [deadLineMinutes, setDeadLineMinutes] = useState(10);
+  const [showModal, setShowModal] = useState(undefined);
+
+  const [inputAmount, setInputAmount] = useState(undefined);
+  const [outputAmount, setOutputAmount] = useState(undefined);
+  const [transaction, setTransaction] = useState(undefined);
+  const [loading, setLoading] = useState(undefined);
+  const [ratio, setRatio] = useState(undefined);
+  const [wethContract, setWethContract] = useState(undefined);
+  const [uniContract, setUniContract] = useState(undefined);
+  const [wethAmount, setWethAmount] = useState(undefined);
+  const [uniAmount, setUniAmount] = useState(undefined);
+
+
   
   useEffect(() => {
     const onLoad = async () => {
       const provider = await new ethers.providers.Web3Provider(window.ethereum);
       setProvider(provider);
+
+      const wethContract = getWethContract();
+      setWethContract(wethContract);
+
+      const uniContract = getUniContract();
+      setUniContract(uniContract);
     }
     onLoad();
   }, [])
@@ -32,6 +60,16 @@ function App() {
      signer.getAddress()
        .then((address) => {
           setSignerAddress(address); 
+
+          wethContract.balanceOf(address)
+           .then(res => {
+             setWethAmount( Number(ethers.utils.formatEther(res)))
+           })
+
+           uniContract.balanceOf(address)
+           .then(res => {
+             setUniAmount( Number(ethers.utils.formatEther(res)))
+           })
        })
   }
   
@@ -68,9 +106,38 @@ function App() {
           <div className='swapContainer'>
             <div className='swapHeader'>
               <span className='swapText'>Swap</span>
-              <span className='gearContainer'>
+              <span className='gearContainer' onClick={() => setShowModal(true)}>
                 <GearFill />
               </span>
+              {
+                showModal && (
+                  <ConfigModal 
+                    onClose={() => setShowModal(false)}
+                    setDeadLineMinutes={setDeadLineMinutes}
+                    deadLineMinutes={deadLineMinutes}
+                    setSlippageAmount={setSlippageAmount}
+                    slippageAmount={slippageAmount}
+                  />
+                )
+              }
+            </div>
+            <div className='swapBody'>
+              <CurrencyField
+                field="input"
+                tokenName="WETH"
+                getSwapPrice={getSwapPrice}
+                signer={signer}
+                balance={wethAmount}
+                />
+                <CurrencyField
+                field="output"
+                tokenName="UNI"
+                value={outputAmount}
+                signer={signer}
+                balance={uniAmount}
+                spinner={BeatLoader}
+                loading={loading}
+                />
             </div>
           </div>
         </div>
---
+++ src/components/Configmodal.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+const ConfigModal = props => {
+    return (
+        <div className='modaly' onClick={props.onClose}>
+            <div className='modal-content' onClick={e => e.stopPropagation()}>
+                <div className='modal-body'>
+                    <h4 className='titleHeader'>Transaction settings</h4>
+
+                    <div className='row'>
+                        <label className='labelField'>Slippage tolerance</label>
+                    </div>
+                    <div className='row'>
+                        <div className='col-md-9 fieldContainer'>
+                            <input
+                                className='inputField'
+                                placeholder='1.0%'
+                                value={props.slippageAmount}
+                                onChange={e => props.setSlippageAmount(e.target.value)}
+                            />
+                        </div>
+                        <div className='col-md-3 inputFieldUnitsContainer'>
+                            <span>%</span>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <label className='labelField'>Transaction Dealine</label>
+                    </div>
+                    <div className='row'>
+                        <div className='col-md-9 fieldContainer'>
+                            <input
+                                className='inputField'
+                                placeholder='1.0%'
+                                value={props.deadlineMinutes}
+                                onChange={e => props.setDeadlineMinutes(e.target.value)}
+                            />
+                        </div>
+                        <div className='col-md-3 inputFieldUnitsContainer'>
+                            <span>minutes</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default ConfigModal
